d9aacbfd1c0a044dd5f0f1379dcef04f3a3aa05f
Sakebul-islam <mdsakebul66@gmail.com>
2024-02-10 22:39:12 +0600

    show user details data from localstorage

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -15,6 +15,15 @@ const UserDetails = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
+        const storedUsers = JSON.parse(localStorage.getItem('users'));
+        if (storedUsers) {
+          const foundUser = storedUsers.find((u) => u.id === parseInt(userId));
+          if (foundUser) {
+            setUser(foundUser);
+            return;
+          }
+        }
+
         const response = await fetch(`https://dummyjson.com/users/${userId}`);
         const data = await response.json();
         setUser(data);
@@ -25,6 +34,7 @@ const UserDetails = () => {
 
     fetchData();
   }, [userId]);
+
   const isEmpty = Object.keys(user).length === 0 && user.constructor === Object;
 
   return (
@@ -35,15 +45,15 @@ const UserDetails = () => {
         <div className='py-12'>
           <div className='container px-2 md:px-8'>
             <div className='mb-8'>
-              <Button variant='outline' size='icon'>
+              <Button variant='outline' size='icon' className='w-28'>
                 <Link
                   to='/'
                   className={`${buttonVariants({
                     variant: 'link',
                     size: 'icon',
-                  })} px-2`}
+                  })} px-2 block`}
                 >
-                  <ChevronLeft className='w-7 inline-block mr-2' />
+                  <ChevronLeft className='w-7 h-7 block mr-2' />
                   Go Home
                 </Link>
               </Button>
